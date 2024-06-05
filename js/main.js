e2ffab14d56a08f3f3e2028dff5a4bcd3b844597
akbarjon <akbarjonsharifjonov33@gmail.com>
2024-06-05 15:11:33 +0500

    Javascript

---
+++ js/main.js
@@ -0,0 +1,74 @@
+let birthDate = prompt ("Tug'ilgan yilingiz qachon ?")
+
+let age = 2024 - birthDate
+
+alert(age)
+
+// {
+//     // masala 1
+//     let a = +prompt('a sonini kiriting')
+//     let p = 4*a
+//     console.log(perimetri:${p});
+// }
+
+// {
+//     // masala 2
+//     let a = +prompt('a son kiriting');
+//     let s = a^2
+//     console.log(yuzasi: ${s});
+// }
+
+// {
+//     // masala 3
+//     let a = +prompt('a son kiriting');
+//     let b = +prompt('b son kiriting');
+//     let s = a*b ;
+//     let p = 2*(a+b);
+//     console.log(yuzasi ${s});
+//     console.log(perimetri:${p});  
+// }
+
+// {
+//     // masala 4
+//     let d = +prompt('d son kiriting');
+//     let l = d* 3.14 ;
+//     console.log(diametri ${l});
+// }
+
+// {
+//     // masala 5
+//     let a = +prompt('a son kiriting');
+//     let v = a*a*a;
+//     let s = 6*a*a;
+//     console.log(hajmi ${v});
+//     console.log(tola sirti ${s});
+// }
+
+// {
+//     // masala 6
+//     let a = +prompt('a sonini kiriting');
+//     let b = +prompt('b sonini kiriting');
+//     let c = +prompt('c sonini kiriting');
+//     let v = a*b*c ;
+//     let s = 2*(a*b+b*c+a*c);
+//     console.log(hajmi ${v});
+//     console.log(tola sirti ${s});
+// }
+
+// {
+//     // masala 7
+//     let r = +prompt('r son kiriting');
+//     let l = 2*3.14*r ;
+//     let s = 3.14*r*r ;
+//     console.log(uzunligi ${l});
+//     console.log(yuzasi ${s});
+
+// }
+
+// {
+//     // masala 8 
+//     let a = +prompt('a son kiriting');
+//     let b = +prompt('b son kiriting');
+//     let result = (a+b)/2;
+//     console.log(orta arifmetigi ${result});
+// }
